98d640012fd8b377698fefc9b9f419ff6e3c694c
5ache3 <mohamedelkhache@gmail.com>
2025-03-22 11:35:11 +0000

    basic sidebar and bottombar

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,18 +1,24 @@
+import Bottombar from '@/components/Bottombar'
+import Sidebar from '@/components/ui/Sidebar'
 import React, { ReactNode } from 'react'
 
 function HomeLayout({children}:{children:ReactNode}) {
   return (
-    <main className='relative'>
-        Navbar
-        <div className='flex'>
-            sidebar
-        </div>
-        <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14 bg-dark-1'>
-            <div className='w-full'>
-                {children}
-            </div>
-        </section>
-    </main>
+    <>
+      <main className='relative'>
+          Navbar
+          <div className='flex'>
+              <Sidebar/>
+              
+              <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14 bg-dark-2'>
+                  <div>
+                      {children}
+                  </div>
+              </section>
+          </div>
+      </main>
+      <Bottombar/>
+    </>
   )
 }
 
---
+++ components/ui/Sidebar.tsx
@@ -0,0 +1,38 @@
+'use client'
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+export default function Sidebar() {
+    const pathname=usePathname()
+  return (
+    <section className='sticky left-0 top-0 flex h-screen w-fit flex-col 
+    justify-between bg-dark-1 text-white p-6 pt-28 max-md:hidden lg:w-[264px]'>
+        <div className='flex flex-col gap-6'>
+            {sidebarLinks.map((link)=>{
+                const isActive = pathname===link.route || (pathname.startsWith(link.route) && link.route.length>1);
+                return (
+                    <Link 
+                    href={link.route}
+                    key={link.label}
+                    className={cn('flex gap-4 items-center p-4 rounded-lg justify-start',{
+                        'bg-blue-500':isActive,
+                    })}>
+                        <Image
+                        src={link.icon}
+                        alt={link.label}
+                        height={24}
+                        width={24}
+                        className='fill-white'
+                        />
+                        <p className='text-lg font-semibold max-lg:hidden'>{link.label}</p>
+                    </Link>
+                )
+            })}
+        </div>
+    </section>
+  )
+}
