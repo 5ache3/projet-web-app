34cb0f46abc35e3102f1acd653da6257698bdf5f
5ache3 <mohamedelkhache@gmail.com>
2025-03-29 14:54:08 +0000

    disable google fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,20 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import { ToastContainer } from "react-toastify";
+// import { Geist, Geist_Mono } from "next/font/google";
 // import "react-toastify/dist/ReactToastify.css";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Variable } from "lucide-react";
 import { Toaster } from "sonner";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -46,7 +45,7 @@ export default function RootLayout({
           },
         }}
       >
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-dark-1`}>
+        <body className={` antialiased bg-dark-1`}>
           {children}
           <Toaster></Toaster>
         </body>
