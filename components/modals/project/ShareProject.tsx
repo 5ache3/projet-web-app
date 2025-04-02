16e377d1141ffa4d76691bd9a2cde16436a63fb6
5ache3 <mohamedelkhache@gmail.com>
2025-04-02 22:34:25 +0000

    copying the project id

--- components/modals/project/ShareProject.tsx
+++ components/modals/project/ShareProject.tsx
@@ -3,14 +3,16 @@ import { Copy } from 'lucide-react'
 import React from 'react'
 
 type promps={
-    id?:string
+    id:string
     title?:string
     isOpen:boolean
     onClose?:()=>void
 }
 export default function ShareProject({id,title,isOpen,onClose}:promps) {
-    const copyToClipboard=()=>{
-        
+    const copyToClipboard= async (str:string)=>{
+        if(str){
+            return await navigator.clipboard.writeText(str)
+        }
     }
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
@@ -20,7 +22,7 @@ export default function ShareProject({id,title,isOpen,onClose}:promps) {
                 <div className='font-semibold'>Use this id to join <span className='text-gray-1'>{title}</span></div>
                 <div className='bg-white text-black font-semibold p-3 rounded-2xl flex justify-between'>
                     <span>{id}</span>
-                    <Copy/>
+                    <Copy className='cursor-pointer' onClick={()=>{copyToClipboard(id)}}/>
                 </div>
             </div>
         </DialogContent>
