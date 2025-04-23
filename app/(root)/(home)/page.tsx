b39e670a663628ad4bd87f05a8c34273e0035d1c
5ache3 <mohamedelkhache@gmail.com>
2025-04-23 08:07:29 +0000

    project skeleton

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import ProjectCard from '@/components/cards/ProjectCard'
+import ListProjectSkeletons from '@/components/reusable/ListProjectSkeletons';
 import HomeActions from '@/components/ui/HomeActions'
 import { Project } from '@/constants/types';
 import { useUser } from '@clerk/nextjs';
@@ -10,6 +11,7 @@ function page() {
   
   const { user } = useUser();
   const [listProjects, setListProjects] = useState<Project[]>([]); 
+  const [loading,setLoading]=useState(true);
   const id = user?.id  ;
   const router = useRouter()
   const cards=['card-1','card-2','card-3','card-4',]
@@ -23,7 +25,8 @@ function page() {
           for(let i=0;i<2&&i<projects.length;i++){
             lis.push(projects[i])
           }
-          setListProjects(lis)
+          setListProjects(lis);
+          setLoading(false);
         }
       } catch (error) {
         console.error("Error fetching projects:", error);
@@ -37,19 +40,24 @@ function page() {
       <div className='p-3 projects bg-mainbg-1  rounded-2xl flex flex-col gap-6 mt-10'>
         <h1 className='text-white px-4 text-3xl font-semibold'>Projects</h1>
         
-        <div className='grid grid-cols-1 gap-4 sm:grid-cols-2  xl:grid-cols-3'>
-          {listProjects.map((item,index) => (
-            <ProjectCard 
-            key={index}
-            title={item.p.title}
-            color={cards[index%cards.length]}
-            total={item.t}
-            completed={item.c}
-            date={item.p.deadline}
-            handleClick={()=>{router.push(`/projects/${item.p.id}`)}}
-            />
-          ))}
+        {loading ? (
+          <ListProjectSkeletons nb={2} />
+        ) : (
+          <div className='grid grid-cols-1 gap-4 sm:grid-cols-2  xl:grid-cols-3'>
+            {listProjects.map((item, index) => (
+              <ProjectCard
+                key={index}
+                title={item.p.title}
+                color={cards[index % cards.length]}
+                total={item.t}
+                completed={item.c}
+                date={item.p.deadline}
+                handleClick={() => router.push(`/projects/${item.p.id}`)}
+              />
+            ))}
         </div>
+        )}
+
       </div>
     </section>
 
--- app/(root)/(home)/projects/page.tsx
+++ app/(root)/(home)/projects/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import ProjectCard from '@/components/cards/ProjectCard'
+import ListProjectSkeletons from '@/components/reusable/ListProjectSkeletons';
 import { GetListProjects } from '@/lib/actions/project.actions'
 import { useUser } from '@clerk/nextjs';
 import { useRouter } from 'next/navigation';
@@ -19,6 +20,7 @@ type project={
 export default function page() {
     const { user } = useUser();
     const [listProjects, setListProjects] = useState<project[]>([]); 
+    const [isLoading,setIsloading]=useState(true)
     const cards=['card-1','card-2','card-3','card-4',]
     const id = user?.id  ;
     const router = useRouter()
@@ -29,12 +31,16 @@ export default function page() {
           const data = await fetch(`/api/user/${id}/projects`);
           const projects = await data?.json()
           setListProjects(projects);
+          setIsloading(false)
         } catch (error) {
           console.error("Error fetching projects:", error);
         }
       };
       fetchProjects();
     }, [id]);
+    if(isLoading){
+      return (<ListProjectSkeletons nb={3}/>)
+    }
   return (
     <div className='text-white'>
       <div className='grid grid-cols-1 gap-4 sm:grid-cols-2  xl:grid-cols-3'>
@@ -50,7 +56,7 @@ export default function page() {
               />
               
             ))}
-              </div>
+      </div>
     </div>
   )
 }
